c0836c3a761c280e3521a5542653c3c875e44ebd
tkck110213 <penetrate.wonder@gmail.com>
2022-01-18 14:29:11 +0900

    fix: debug
    動く状態に戻した

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include "include/Prottype.hpp"
-#include <time.h>
-void export_2nd_csv(int n, vector<vector<float>> &data, string ResultPath, string filename);
 
 void export_graph(int n, int time, SNS &sns, array<UserAgent, N_user> users, array<MediaAgent, N_media> media, string ResultPath){
     stringstream ResultGraphRelationPath;
@@ -50,29 +48,6 @@ void export_graph(int n, int time, SNS &sns, array<UserAgent, N_user> users, arr
     }
 }
 
-void export_2nd_csv(int n, vector<vector<float>> &data, string ResultPath, string filename){
-    stringstream Result2ndCsvPath;
-    Result2ndCsvPath << ResultPath << "data/";
-    Result2ndCsvPath << follow_method << "_";
-    Result2ndCsvPath << "c" << confidence_level << "_";
-    Result2ndCsvPath << "mf" << media_follower << "_";
-    Result2ndCsvPath << "n" << n << "_";
-    Result2ndCsvPath << filename << ".csv";
-
-    ofstream Result2ndCsvData(Result2ndCsvPath.str());
-    int RowSize = data.size();
-    int ColSize = data.at(0).size();
-
-    for(int i = 0; i < RowSize; ++i){
-        for(int j = 0; j < ColSize; ++j){
-            Result2ndCsvData << data.at(i).at(j) << ",";
-        }
-        Result2ndCsvData << endl;
-    }
-
-}
-
-
 string get_date(void){
     //現在日時を取得する
     time_t t = time(nullptr);
@@ -95,7 +70,6 @@ string get_date(void){
 }
 
 int main(void) {
-    clock_t start = clock();
     random_seed(seed); 
     string ResultPath = "./result/" + get_date() + "/";
 
@@ -105,52 +79,27 @@ int main(void) {
     filesystem::create_directories(ResultPath + "data/");
     filesystem::create_directories(ResultPath + "gexf/");
 
-    //max_n回シミュレーションを実行する
     for(int n = 0; n < max_n; ++n){
         cout << "n " << n << endl;
         SNS sns(N, E);
         array<UserAgent, N_user> users;
         array<MediaAgent, N_media> media;
-        //全ステップの全ユーザの意見
-        vector<vector<float>> all_step_opinions(N, vector<float>(T));
-        float opinion_range[amount_type_opinion_range*media_per_opinion_range][2];
         
         //人数分のユーザエージェントを作成
         for(int i = 0; i < N_user; ++i){
             users[i].initialize(i);
         }
-        //人数分のopinion rangeの作成
-        //opinion rangeの各種類を順番に渡していく
-        for(int i = 0; i < N_media; i += media_per_opinion_range){
-            for(int j = 0; j < amount_type_opinion_range; ++j){
-                opinion_range[i + j][0] = opinion_ranges[i][0];
-                opinion_range[i + j][1] = opinion_ranges[i][1];
-            }
-        }
-        
-
         //人数分のメディアエージェントを作成
         for(int i = 0; i < N_media; ++i){
-            media[i].initialize(i + N_user, opinion_range[i]);  
+            media[i].initialize(i + N_user);
         }
         
-        //シミュレーションをstep Tまで実行
         for(int time = 0; time < T; ++time){
             
-            //グラフの出力
             if(time % 2000 == 0){
                 export_graph(n, time, sns, users, media, ResultPath);
             }
-            //各エージェントのopinionの履歴を保存
-            
-            for(int i = 0; i < N_user; ++i){//usersの添え字はN_userまで
-                all_step_opinions.at(i).at(time) = users[i].o;
-            }
-            for(int i = 0; i < N_media; ++i){
-                all_step_opinions.at(i + N_user).at(time) = media[i].o;
-            }
-            
-            //シミュレーション開始
+
             int user = random_int(0, N_user - 1);
             int medium = random_int(0, N_media - 1);
             vector<Message> similar_post;
@@ -161,22 +110,21 @@ int main(void) {
             users[user].post(time, sns, similar_post);
             users[user].refollow(sns, unsimilar_post);
             
-            //シミュレーションの折り返し地点でメディアのopinion rangeを変更
             if(time == T/2){
                 for(int m = 0; m < N_media; ++m){
                     media[m].change_opinion_range(-0.1, 0.1);
+                    //printf("range0 %f\n", media[m].opinion_range[0]);
+                    //printf("range1 %f\n", media[m].opinion_range[1]);
+                    //printf("opinion %f\n", media[m].o);
                 }
             }
+
             media[medium].post(time, sns);
-            printf("flag\n");
+
         }
-        
+
         export_graph(n, T, sns, users, media, ResultPath);
-        
-        //export_2nd_csv(n, all_step_opinions, ResultPath, "all_step_opinions");
-        
     }
-    clock_t end = clock();
-    cout << "duration = " << (double)(end - start) / CLOCKS_PER_SEC << "sec.\n";
+
     return 0;
 }
