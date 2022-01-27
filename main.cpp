97df2c2b0b19232955fde3ce1d86e097a1617415
tkck110213 <penetrate.wonder@gmail.com>
2022-01-27 22:35:40 +0900

    fix:json import
    引数からとったパスを指定し忘れていたので、直す

--- main.cpp
+++ main.cpp
@@ -218,7 +218,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     //パラメータが書かれたjsonファイルを引数に取る
-    ifstream ifs("./parameter.json");
+    ifstream ifs(argv[1]);
     json parameter;
     ifs >> parameter;
 
